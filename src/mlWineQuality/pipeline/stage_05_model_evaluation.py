eaf89510eeb400a0bf27a566ca7379e0f74a3655
Darshana Dinushal <darshana.dinushal@2mnordic.com>
2025-02-13 23:07:01 +0100

    add model evaluation

---
+++ src/mlWineQuality/pipeline/stage_05_model_evaluation.py
@@ -0,0 +1,15 @@
+from mlWineQuality.config.configuration import ConfigurationManager
+from mlWineQuality.components.model_evaluation import ModelEvaluation
+from mlWineQuality import logger
+
+STAGE_NAME = "Model evaluation stage"
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.save_results()
